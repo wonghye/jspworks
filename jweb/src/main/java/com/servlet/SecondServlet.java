c729bcfcfadee2cd4f0260f02c4dd73c55964efa
wonghye <dearhw117@naver.com>
2022-08-05 17:17:24 +0900

    0805

--- jweb/src/main/java/com/servlet/SecondServlet.java
+++ jweb/src/main/java/com/servlet/SecondServlet.java
@@ -1,32 +1,33 @@
 package com.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+@WebServlet("/dir/second")
 public class SecondServlet extends HttpServlet {
-
 	private static final long serialVersionUID = 1L;
-	
-	@Override
-	public void init() throws ServletException {
-		System.out.println("init() 호출");
-	}
-	
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("doGet() 호출");
-	}
 
-	@Override
-	public void destroy() {
-		System.out.println("destroy() 호출");
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html><body>");
+		out.println("sendRedirect를 이용한 redirect 실습");
+		out.println("</body></html>");
 	}
 
 	
-	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 
 }
